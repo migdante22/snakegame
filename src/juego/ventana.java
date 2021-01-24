7a3148dc152a29c8ac851b03cfcf886f4c99c284
migdante22 <art.angel222@gmail.com>
2021-01-24 18:11:39 -0500

    punto snake
    se crea panel para dibujar y dibujo de snake

--- src/juego/ventana.java
+++ src/juego/ventana.java
@@ -1,20 +1,31 @@
 package juego;
 
 import java.awt.Canvas;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.sun.glass.events.KeyEvent;
 
 public class ventana extends JFrame{
 	
-Canvas panelCanvas;
+
+
+Point snake;
+int snakeancho = 50;
+int snakealto = 10;
+imaSnake ImaSnake;
 	
-private int ancho = 600;
+private int ancho = 800;
 private int alto = 400;
 
 	public ventana () {
 		
-		panelCanvas = new Canvas();
+
+		
+		teclas teclas = new teclas();
 		
 		
 		setSize(ancho, alto);
@@ -23,14 +34,55 @@ private int alto = 400;
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		add(panelCanvas);
+		addKeyListener(teclas);
 		
 		
+		snake = new Point(ancho/2, alto/2);
 		
+		ImaSnake = new imaSnake();
+		this.getContentPane().add(ImaSnake);
+
+		
+	
+		
+
+			
 	}
+	
 
 	public static void main(String[] args) {
 		ventana v = new ventana();
+		
+		
 	}
+	
+	
+	// panel donde se dibuja el juego
+	public class imaSnake extends JPanel{
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(Color.BLUE);
+			g.fillRect(ancho/2, alto/2, 20, 20);
+		}
+	}
+	
+	
+	
+	
+	public class teclas extends KeyAdapter{
+		@Override
+		public void keyPressed(java.awt.event.KeyEvent e) {
+			super.keyPressed(e);
+			
+			if (e.getKeyCode() == java.awt.event.KeyEvent.VK_ESCAPE) {
+				System.exit(0);
+			}
+		}
+		
+	}
+		
+
 
 }
+
