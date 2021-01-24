5888a20aa12956d6367506bc778b699c7e3408a6
migdante22 <art.angel222@gmail.com>
2021-01-24 13:26:52 -0500

    inicio

---
+++ src/juego/ventana.java
@@ -0,0 +1,14 @@
+package juego;
+
+public class ventana {
+
+	public ventana() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
